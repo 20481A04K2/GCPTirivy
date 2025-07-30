5361f2776e975dd44f9063db25e66d15cbcde986
SAJJA VAMSI <99644384+20481A04K2@users.noreply.github.com>
2025-07-30 15:20:09 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from Flask App in Artifact Registry!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
