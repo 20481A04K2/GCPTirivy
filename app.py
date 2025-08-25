4d482ef4ed6e0aaec46da76ddc04b927dc6bc01b
SAJJA VAMSI <99644384+20481A04K2@users.noreply.github.com>
2025-08-25 09:48:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask
-
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Hello from Flask App in Artifact Registry!"
+    aws_access = os.getenv("AWS_ACCESS_KEY", "Not Found")
+    aws_secret = os.getenv("AWS_SECRET_KEY", "Not Found")
 
-if __name__ == '__main__':
- 
-    app.run(host='0.0.0.0', port=6000)
+    return f"""
+    <h1>Secrets in Cloud Run</h1>
+    <p><b>AWS_ACCESS_KEY:</b> {aws_access}</p>
+    <p><b>AWS_SECRET_KEY:</b> {aws_secret}</p>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
